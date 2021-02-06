98dd01d53b0f3f0172f5beb46509ce24ce18714a
mohanad <mohanad_1x@yahoo.com>
2021-02-06 21:35:30 +0200

    hospatil

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -115,6 +115,23 @@
                     </div>
                 </div>
             @endif
+            @if(permission_show('hospatil-index'))
+                <div class="col-lg-3 col-xs-6">
+                    <!-- small box -->
+                    <div class="small-box bg-blue">
+                        <div class="inner">
+                            <h3>{{$count_hospatil}}</h3>
+
+                            <p>{{trans('lang.Hospatil')}}</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-person"></i>
+                        </div>
+                        <a href="{{ url('/admin/hospatil/index') }}" class="small-box-footer">{{trans('lang.Index')}} <i
+                                    class="fa fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+            @endif
         <!-- ./col -->
         </div>
     {{--<div class="row">
